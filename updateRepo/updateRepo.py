81c6cb97ea71cbaba921f37028429628bc22a449
Nicolas Godoy Hassan <ngodoyhassan@gmail.com>
2023-07-25 18:07:06 -0300

    updated via updateRepo

--- updateRepo/updateRepo.py
+++ updateRepo/updateRepo.py
@@ -6,10 +6,16 @@ def cmd(cmd):
     response = subprocess.run(cmd, shell=True)
     return response
 
+print()
 cmd('git pull')
 
+print()
 cmd('git add .')
+
+print()
 cmd('git commit -m "updated via updateRepo"')
+
+print()
 cmd('git push origin main')
 
 print("Press any key to exit.")
